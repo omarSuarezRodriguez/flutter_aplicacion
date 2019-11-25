36f0974da0535aa6a13ecdd3adb4da3570203eeb
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-11-25 02:34:24 -0500

    commit

---
+++ lib/home.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'gallery.dart';
+import 'import.dart';
+import 'export.dart';
+import 'profile.dart';
+import 'logout.dart';
+
+class Home extends StatefulWidget {
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  int _selectDrawerItem = 0;
+  _getDrawerItemWidget(int pos) {
+    switch(pos) {
+      case 0: return Import();
+      case 1: return Gallery();
+      case 2: return Export();
+      case 3: return Profile();
+      case 4: return Logout();
+    }
+  }
+
+
+  _onSelectItem(int pos) {
+    Navigator.of(context).pop();
+    setState(() {
+      _selectDrawerItem = pos;
+    });
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Navigation Drawer'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text('Omar Suarez'),
+              accountEmail: Text('omar.suarez.rod@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Text('O', style: TextStyle(fontSize: 40.0)),
+              ),
+            ),
+            ListTile(
+              title: Text('Import'),
+              leading: Icon(Icons.camera_alt),
+              selected: (0 == _selectDrawerItem),
+              onTap: () {
+                _onSelectItem(0);
+              },
+            ),
+            ListTile(
+              title: Text('Gallery'),
+              leading: Icon(Icons.photo_library),
+              selected: (1 == _selectDrawerItem),
+              onTap: () {
+                _onSelectItem(1);
+              },
+            ),
+            ListTile(
+              title: Text('Export'),
+              leading: Icon(Icons.save),
+              selected: (2 == _selectDrawerItem),
+              onTap: () {
+                _onSelectItem(2);
+              },
+            ),
+            Divider(),
+            ListTile(
+              title: Text('Profile'),
+              leading: Icon(Icons.account_circle),
+              selected: (3 == _selectDrawerItem),
+              onTap: () {
+                _onSelectItem(3);
+              },
+            ),
+            ListTile(
+              title: Text('Logout'),
+              leading: Icon(Icons.exit_to_app),
+              selected: (4 == _selectDrawerItem),
+              onTap: () {
+                _onSelectItem(4);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: _getDrawerItemWidget(_selectDrawerItem),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+
+void main() => runApp(Aplication());
+
+
+class Aplication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigation Drawer',
+      home: Home(),
+      theme: ThemeData(
+        primaryColor: Color(0xFF2F008E),
+        accentColor: Color(0xFFFDD303)
+      )
+    );
+  }
+
+}
