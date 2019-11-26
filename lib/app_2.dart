04b0250155a7e3a24ed1baef1903f8e0e3aec07e
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-11-26 11:41:07 -0500

    commit

--- lib/app_2.dart
+++ lib/app_2.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 class App_2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('App 2', style: TextStyle(fontSize: 40.0)),
+    //TODO el código
+    return ListView(
+      children: <Widget>[
+        Text("Prueba",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 30, color: Colors.blue)),
+      ],
     );
   }
 }
