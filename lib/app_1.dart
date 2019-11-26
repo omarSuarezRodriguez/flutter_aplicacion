1d7c316b0dbae8230cca2df192b046f3ab0c93c2
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-11-26 10:15:50 -0500

    commit

--- lib/app_1.dart
+++ lib/app_1.dart
@@ -3,27 +3,68 @@ import 'package:flutter/material.dart';
 class App_1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-            title: new Text("Input text"), backgroundColor: Color(0xFF54B5F6)),
-        body: Container(
-            child: Center(
-                child: Column(
-          children: <Widget>[
-            TextField(
-              decoration: new InputDecoration(hintText: "Type in here!"),
+    final miControlador = TextEditingController();
+
+    return ListView(
+      padding: EdgeInsets.all(20),
+      children: <Widget>[
+        Text(
+          "App 1",
+          textAlign: TextAlign.center,
+        ),
+        TextFormField(
+          controller: miControlador,
+          decoration: InputDecoration(
+            labelText: 'Ingrese el primer número',
+          ),
+        ),
+        Text(miControlador.text),
+        RaisedButton(
+          onPressed: (){
+            showMyDialog(context, miControlador.text);
+          },
+        ),
+      ],
+    );
+  }
+
+  //Mostrar showDialog
+  void showMyDialog(BuildContext context, String texto) {
+    showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Text(
+            texto,
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
             ),
-            Text("Hola"),
-            Text("Hola mundo"),
-            Text("Hola"),
-            Text("Hola"),
-            Text("Hola"),
-            Text("Hola"),
-            TextField(
-              decoration: new InputDecoration(hintText: "Type in here!"),
-            ),
-            Text("Hola"),
           ],
-        ))));
+        );
+      },
+    );
   }
 }
+
+//Código comentareado:
+
+//TextField Normal:
+//
+//TextField(
+//decoration: InputDecoration(
+////            border: InputBorder.none,
+//hintText: 'Please enter a search term'),
+//),
+
+//TextField con ayuda o TextFormField
+//
+//TextFormField(
+//decoration: InputDecoration(
+//labelText: 'Enter your Username',
+//),
+//),
